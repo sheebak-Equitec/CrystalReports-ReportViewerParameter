c0c31f0e8d8ab1d8830b57479ebfe8b507a59b07
sheeba-khan <124671004+sheebak-Equitec@users.noreply.github.com>
2024-12-18 12:41:12 +0530

    Initial commit

---
+++ CrystalReportViewer/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CrystalReportViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Margin = new System.Windows.Forms.Padding(4);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1469, 772);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ToolPanelWidth = 267;
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = 0;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.ReportSource = "C:\\Users\\Equilap41\\Documents\\GitHub\\HRMS\\BBK-HRMS-MVC\\HRMS\\Reports\\M5\\rptM5_ListM" +
+    "nthDistribution.rpt";
+            this.crystalReportViewer2.Size = new System.Drawing.Size(1469, 772);
+            this.crystalReportViewer2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1469, 772);
+            this.Controls.Add(this.crystalReportViewer2);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+    }
+}
+
---
+++ CrystalReportViewer/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace CrystalReportViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load the Crystal Report
+            LoadCrystalReport();
+        }
+
+        private void LoadCrystalReport()
+        {
+            try
+            {
+                string reportPath = @"C:\Users\Equilap41\Documents\GitHub\HRMS\BBK-HRMS-MVC\HRMS\Reports\M5\rptM5_ListMnthDistribution.rpt";
+
+                if (!System.IO.File.Exists(reportPath))
+                {
+                    MessageBox.Show("Report file not found at: " + reportPath);
+                    return;
+                }
+
+                ReportDocument reportDocument = new ReportDocument();
+                reportDocument.Load(reportPath);
+
+                Dictionary<string, object> parameters = new Dictionary<string, object>
+                {
+                    { "@i_CompanyCd", "BBK" },
+                    { "@i_GroupCd", " " },
+                    { "@i_DivisionCd", "" },
+                    { "@i_DepartmentCd", "" },
+                    { "@i_SectionCd", "" },
+                    { "@i_Emp_No", "1763" },
+                    { "@i_For_Month", "01/01/2021" },
+                    { "@i_Incharger_Emp_No", "1763" },
+                    { "@i_Status", "A" },
+                };
+
+                foreach (KeyValuePair<string, object> param in parameters)
+                {
+                    reportDocument.SetParameterValue(param.Key, param.Value);
+                }
+
+                crystalReportViewer1.ReportSource = reportDocument;
+                crystalReportViewer1.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void crystalReportViewer2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
